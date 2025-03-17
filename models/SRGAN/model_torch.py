b553f87dbcd10e34fd9a0d05f75130ba5b0bb188
RongWangRong <2687458039@qq.com>
2025-03-17 15:49:46 +0800

    first commit

--- models/SRGAN/model_torch.py
+++ models/SRGAN/model_torch.py
@@ -1,138 +1,191 @@
 import math
 import torch
-from torch import nn
-import argparse
+import torch.nn as nn
 
-class Generator(nn.Module):
-    def __init__(self, scale_factor):
-        upsample_block_num = int(math.log(scale_factor, 2))
-
-        super(Generator, self).__init__()
-        self.block1 = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=9, padding=4),
-            nn.PReLU()
-        )
-        self.block2 = ResidualBlock(64)
-        self.block3 = ResidualBlock(64)
-        self.block4 = ResidualBlock(64)
-        self.block5 = ResidualBlock(64)
-        self.block6 = ResidualBlock(64)
-        self.block7 = nn.Sequential(
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64)
-        )
-        block8 = [UpsampleBLock(64, 2) for _ in range(upsample_block_num)]
-        block8.append(nn.Conv2d(64, 3, kernel_size=9, padding=4))
-        self.block8 = nn.Sequential(*block8)
-
-    def forward(self, x):
-        block1 = self.block1(x)
-        block2 = self.block2(block1)
-        block3 = self.block3(block2)
-        block4 = self.block4(block3)
-        block5 = self.block5(block4)
-        block6 = self.block6(block5)
-        block7 = self.block7(block6)
-        block8 = self.block8(block1 + block7)
-
-        return (torch.tanh(block8) + 1) / 2
-
-
-class Discriminator(nn.Module):
-    def __init__(self):
-        super(Discriminator, self).__init__()
-        self.net = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, padding=1),
-            nn.LeakyReLU(0.2),
-
-            nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm2d(64),
-            nn.LeakyReLU(0.2),
-
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.BatchNorm2d(128),
-            nn.LeakyReLU(0.2),
-
-            nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm2d(128),
-            nn.LeakyReLU(0.2),
-
-            nn.Conv2d(128, 256, kernel_size=3, padding=1),
-            nn.BatchNorm2d(256),
-            nn.LeakyReLU(0.2),
-
-            nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm2d(256),
-            nn.LeakyReLU(0.2),
-
-            nn.Conv2d(256, 512, kernel_size=3, padding=1),
-            nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.2),
-
-            nn.Conv2d(512, 512, kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.2),
-
-            nn.AdaptiveAvgPool2d(1),
-            nn.Conv2d(512, 1024, kernel_size=1),
-            nn.LeakyReLU(0.2),
-            nn.Conv2d(1024, 1, kernel_size=1)
-        )
-
-    def forward(self, x):
-        batch_size = x.size(0)
-        return torch.sigmoid(self.net(x).view(batch_size))
-
+def init_weights(net, init_type='normal', init_gain=0.02):
+    """Initialize network weights.
+    init_type: normal | xavier | kaiming
+    init_gain: scaling factor for normal, xavier and kaiming.
+    """
+    for m in net.modules():
+        if isinstance(m, nn.Conv2d):
+            if init_type == 'normal':
+                nn.init.normal_(m.weight.data, 0.0, init_gain)
+            elif init_type == 'xavier':
+                nn.init.xavier_normal_(m.weight.data, gain=init_gain)
+            elif init_type == 'kaiming':
+                nn.init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+            else:
+                raise NotImplementedError(f'initialization method {init_type} is not implemented')
+            if m.bias is not None:
+                nn.init.constant_(m.bias.data, 0.0)
+        elif isinstance(m, nn.BatchNorm2d):
+            nn.init.constant_(m.weight.data, 1.0)
+            nn.init.constant_(m.bias.data, 0.0)
 
 class ResidualBlock(nn.Module):
+    """Structure of ResidualBlock"""
     def __init__(self, channels):
         super(ResidualBlock, self).__init__()
-        self.conv1 = nn.Conv2d(channels, channels, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv2d(channels, channels, kernel_size=3, stride=1, padding=1, bias=True)
         self.bn1 = nn.BatchNorm2d(channels)
-        self.prelu = nn.PReLU()
-        self.conv2 = nn.Conv2d(channels, channels, kernel_size=3, padding=1)
+        self.prelu = nn.PReLU(num_parameters=channels)
+        self.conv2 = nn.Conv2d(channels, channels, kernel_size=3, stride=1, padding=1, bias=True)
         self.bn2 = nn.BatchNorm2d(channels)
 
     def forward(self, x):
-        residual = self.conv1(x)
-        residual = self.bn1(residual)
-        residual = self.prelu(residual)
-        residual = self.conv2(residual)
-        residual = self.bn2(residual)
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.prelu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        return out + x
 
-        return x + residual
-
-
-class UpsampleBLock(nn.Module):
-    def __init__(self, in_channels, up_scale):
-        super(UpsampleBLock, self).__init__()
-        self.conv = nn.Conv2d(in_channels, in_channels * up_scale ** 2, kernel_size=3, padding=1)
-        self.pixel_shuffle = nn.PixelShuffle(up_scale)
-        self.prelu = nn.PReLU()
+class SubpixelConvolutionLayer(nn.Module):
+    """Structure of SubpixelConvolutionLayer"""
+    def __init__(self, channels):
+        super(SubpixelConvolutionLayer, self).__init__()
+        self.conv = nn.Conv2d(channels, channels * 4, kernel_size=3, stride=1, padding=1, bias=True)
+        self.pixel_shuffle = nn.PixelShuffle(upscale_factor=2)
+        self.prelu = nn.PReLU(num_parameters=channels)
 
     def forward(self, x):
-        x = self.conv(x)
-        x = self.pixel_shuffle(x)
-        x = self.prelu(x)
-        return x
+        out = self.conv(x)
+        out = self.pixel_shuffle(out)
+        out = self.prelu(out)
+        return out
 
+class Generator(nn.Module):
+    """Structure of Generator"""
+    def __init__(self, upscale_factor):
+        super(Generator, self).__init__()
+        # 计算子像素卷积层的数量
+        num_subpixel_convolution_layers = int(math.log(upscale_factor, 2))
+        # 第一层
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=9, stride=1, padding=4, bias=True),
+            nn.PReLU(num_parameters=64)
+        )
 
-parser = argparse.ArgumentParser(description='Train Super Resolution Models')
-parser.add_argument('--crop_size', default=88, type=int, help='training images crop size')
-parser.add_argument('--upscale_factor', default=4, type=int, choices=[2, 4, 8],
-                    help='super resolution upscale factor')
-parser.add_argument('--num_epochs', default=100, type=int, help='train epoch number')
+        # 16个残差块
+        trunk = []
+        for _ in range(16):
+            trunk.append(ResidualBlock(64))
+        self.trunk = nn.Sequential(*trunk)
 
+        # 残差块后的第二个卷积层
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.PReLU(num_parameters=64)
+        )
 
-if __name__ == '__main__':
-    opt = parser.parse_args()
-    
-    CROP_SIZE = opt.crop_size
-    UPSCALE_FACTOR = opt.upscale_factor
-    NUM_EPOCHS = opt.num_epochs
+        # 子像素卷积层
+        subpixel_conv_layers = []
+        for _ in range(num_subpixel_convolution_layers):
+            subpixel_conv_layers.append(SubpixelConvolutionLayer(64))
+        self.subpixel_conv = nn.Sequential(*subpixel_conv_layers)
 
-    netG = Generator(UPSCALE_FACTOR)
-    print('# generator parameters:', sum(param.numel() for param in netG.parameters()))
-    netD = Discriminator()
-    print('# discriminator parameters:', sum(param.numel() for param in netD.parameters()))
+        # 最后的输出层
+        self.conv3 = nn.Conv2d(64, 3, kernel_size=9, stride=1, padding=4, bias=True)
+        self.tanh = nn.Tanh()
 
+    def forward(self, x):
+        conv1 = self.conv1(x)
+        trunk = self.trunk(conv1)
+        conv2 = self.conv2(trunk)
+        out = conv1 + conv2
+        out = self.subpixel_conv(out)
+        out = self.conv3(out)
+        out = self.tanh(out)
+        return out
+
+def get_generator(upscale_factor, init_gain):
+    """Return generator network by args."""
+    net = Generator(upscale_factor)
+    init_weights(net, init_type='normal', init_gain=init_gain)
+    return net
+
+import torch
+import torch.nn as nn
+
+def init_weights(net, init_type='normal', init_gain=0.02):
+    """Initialize network weights.
+    init_type: normal | xavier | kaiming
+    init_gain: scaling factor for normal, xavier and kaiming.
+    """
+    for m in net.modules():
+        if isinstance(m, nn.Conv2d):
+            if init_type == 'normal':
+                nn.init.normal_(m.weight.data, 0.0, init_gain)
+            elif init_type == 'xavier':
+                nn.init.xavier_normal_(m.weight.data, gain=init_gain)
+            elif init_type == 'kaiming':
+                nn.init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+            else:
+                raise NotImplementedError(f'initialization method {init_type} is not implemented')
+            if m.bias is not None:
+                nn.init.constant_(m.bias.data, 0.0)
+        elif isinstance(m, nn.BatchNorm2d):
+            nn.init.constant_(m.weight.data, 1.0)
+            nn.init.constant_(m.bias.data, 0.0)
+
+class Discriminator(nn.Module):
+    """Structure of Discriminator"""
+    def __init__(self, image_size):
+        super(Discriminator, self).__init__()
+        feature_map_size = int(image_size // 16)
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128, eps=1e-05),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(128, eps=1e-05),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256, eps=1e-05),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=1),  # state size: (256) x (image_size/8) x (image_size/8)
+            nn.BatchNorm2d(256, eps=1e-05),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512, eps=1e-05),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(512, 512, kernel_size=3, stride=2, padding=1),  # state size: (512) x (image_size/16) x (image_size/16)
+            nn.BatchNorm2d(512, eps=1e-05),
+            nn.LeakyReLU(0.2),
+        )
+        self.flatten = nn.Flatten()
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * feature_map_size * feature_map_size, 1024),
+            nn.LeakyReLU(0.2),
+            nn.Linear(1024, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        out = self.features(x)
+        out = self.flatten(out)
+        out = self.classifier(out)
+        return out
+
+def get_discriminator(image_size, init_gain):
+    """Return discriminator by args."""
+    net = Discriminator(image_size)
+    init_weights(net, 'normal', init_gain)
+    return net
+
+if __name__ == "__main__":
+
+    print(get_generator(4, 0.02))
+    print(get_discriminator(4, 0.02))
